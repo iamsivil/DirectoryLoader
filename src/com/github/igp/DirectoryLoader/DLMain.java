5095f85457b6b36c2430575312fd975d69edfea5
Thomas Johnson <xenofixus@gmail.com>
2012-03-26 08:56:18 -0400

    switching to c#/c++ style formatting

--- src/com/github/igp/DirectoryLoader/DLMain.java
+++ src/com/github/igp/DirectoryLoader/DLMain.java
@@ -8,31 +8,39 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.UnknownDependencyException;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DLMain extends JavaPlugin {
+public class DLMain extends JavaPlugin
+{
 	private Logger log;
 
 	@Override
-	public void onEnable() {
+	public void onEnable()
+	{
 		log = this.getLogger();
 		final DLPluginLoader pluginLoader = new DLPluginLoader(getServer());
 
-		for (final String directory : getConfig().getStringList("directories")) {
+		for (final String directory : getConfig().getStringList("directories"))
+		{
 			final File f = new File(directory);
 			Plugin plugin = null;
 
-			try {
+			try
+			{
 				plugin = pluginLoader.loadPlugin(f);
 			}
-			catch (final InvalidPluginException e) {
+			catch (final InvalidPluginException e)
+			{
 				log.severe("Invalid plugin at: " + f.getAbsolutePath());
 			}
-			catch (final UnknownDependencyException e) {
+			catch (final UnknownDependencyException e)
+			{
 				log.severe("Unable to load dependency for plugin at: " + f.getAbsolutePath());
 			}
-			finally {
+			finally
+			{
 				if (plugin == null)
 					log.severe("Error enabling plugin at: " + f.getAbsolutePath());
-				else {
+				else
+				{
 					log.info("Loading " + plugin.getDescription().getFullName());
 					getServer().getPluginManager().enablePlugin(plugin);
 				}
@@ -42,7 +50,8 @@ public class DLMain extends JavaPlugin {
 	}
 
 	@Override
-	public void onDisable() {
+	public void onDisable()
+	{
 		log.info("Disabled.");
 	}
 }
--- src/com/github/igp/DirectoryLoader/DLPluginLoader.java
+++ src/com/github/igp/DirectoryLoader/DLPluginLoader.java
@@ -15,20 +15,24 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPluginLoader;
 import org.yaml.snakeyaml.error.YAMLException;
 
-public class DLPluginLoader extends JavaPluginLoader {
+public class DLPluginLoader extends JavaPluginLoader
+{
 	@SuppressWarnings("unused")
 	private final Server server;
-	protected final Pattern[] fileFilters = {
+	protected final Pattern[] fileFilters =
+	{
 		Pattern.compile(".$")
 	};
 
-	public DLPluginLoader(final Server instance) {
+	public DLPluginLoader(final Server instance)
+	{
 		super(instance);
 		server = instance;
 	}
 
 	@Override
-	public Plugin loadPlugin(final File file) throws InvalidPluginException {
+	public Plugin loadPlugin(final File file) throws InvalidPluginException
+	{
 
 		if (!file.isDirectory())
 			throw new InvalidPluginException("Not a directory");
@@ -37,9 +41,11 @@ public class DLPluginLoader extends JavaPluginLoader {
 	}
 
 	@Override
-	public PluginDescriptionFile getPluginDescription(final File file) throws InvalidDescriptionException {
+	public PluginDescriptionFile getPluginDescription(final File file) throws InvalidDescriptionException
+	{
 		InputStream stream = null;
-		try {
+		try
+		{
 			final File desc = new File(file.getAbsolutePath() + File.separator + "plugin.yml");
 
 			if ((desc == null) || (!desc.exists()))
@@ -49,25 +55,32 @@ public class DLPluginLoader extends JavaPluginLoader {
 
 			return new PluginDescriptionFile(stream);
 		}
-		catch (final IOException ex) {
+		catch (final IOException ex)
+		{
 			throw new InvalidDescriptionException(ex);
 		}
-		catch (final YAMLException ex) {
+		catch (final YAMLException ex)
+		{
 			throw new InvalidDescriptionException(ex);
 		}
-		finally {
-			if (stream != null) {
-				try {
+		finally
+		{
+			if (stream != null)
+			{
+				try
+				{
 					stream.close();
 				}
-				catch (final IOException e) {
+				catch (final IOException e)
+				{
 				}
 			}
 		}
 	}
 
 	@Override
-	public Pattern[] getPluginFileFilters() {
+	public Pattern[] getPluginFileFilters()
+	{
 		return this.fileFilters;
 	}
 }
